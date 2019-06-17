d7f890da3ec8e599c6a5b38895d78bcec40a5be0
Meera Madhavan <meeram2008@gmail.com>
2019-06-16 20:45:02 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,14 +30,16 @@ var cardsInPlay=[];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
+  		alert("You found a match!");
 	} 
 	else {
-  		console.log("Sorry, try again.");
+  		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+	var cardId=this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -51,6 +53,14 @@ function flipCard(cardId){
 
 }
 
+function createBoard(){
+	for (i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
