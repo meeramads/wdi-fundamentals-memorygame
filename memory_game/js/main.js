c0822e0e764118455faedbb396ad9b858cc0b493
Meera Madhavan <meeram2008@gmail.com>
2019-06-16 19:00:29 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,26 @@
-console.log("Up and running!");
-
 var cards= ["queen", "queen", "king", "king"];
 var cardsInPlay=[];
-var cardOne=cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
 
-if(cardsInPlay.length = 2)
-{
-	if (cardsInPlay[0]===cardsInPlay[1]) {
-		alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} 
+	else {
+  		console.log("Sorry, try again.");
 	}
-	else{
-		alert("Sorry, try again.");
-	}		
+}
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2)
+	{
+		checkForMatch();	
+	}
+
+
 }
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
